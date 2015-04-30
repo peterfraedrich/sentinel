f080290a140fb5be67d65376b8535d53d4bc6986
Peter Fraedrich <peter.fraedrich@coldblue-usa.com>
2015-04-30 18:06:10 -0400

    add client auth

--- server.js
+++ server.js
@@ -55,7 +55,7 @@
     // ============= API ======================= //
 
     app.post('/', function (req, res) {
-      var jsonData = JSON.parse(req.body);
+      var jsonData = JSON.parse(req.body.mydata);
       console.log(jsonData);
       res.end('OK');
       auth.auth.findone({'clientID': jsonData.clientID}, function(req, client) {
