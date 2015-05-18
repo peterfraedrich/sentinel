5afcba8d5b8cabd890141522a8794e050937a951
Peter Fraedrich <peter.fraedrich@coldblue-usa.com>
2015-05-18 15:42:24 -0400

    data changes

--- server.js
+++ server.js
@@ -89,11 +89,16 @@
             res.writeHead(200, {'Content-Type' : 'application/json'});
             console.log(record.ipaddr)
             fs.openSync('form.html', 'r', function (err, data) {
+              if (err) {
+                throw err;
+                res.end();
+              } else {
               fs.readFileSync('form.html', function (err, data) {
                 if (err) throw err;
                 console.log(data);
+                res.end();
               });
-
+             };
             });
             
             //res.end(str);
